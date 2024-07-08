7ab4b03c6cd83cdb8b5446ed5422bc098708c24d
claudiahW <murungiclaud@gmail.com>
2024-07-08 17:36:30 +0300

    add logo to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import logo from '../../assets/bird_2.jpg';
 
 const Navbar = () => {
   return (
   <div className="navbar">
-
+<img src={logo} alt="" />
     
-    Navbar</div>)
+  </div>)
 };
 
 export default Navbar;
