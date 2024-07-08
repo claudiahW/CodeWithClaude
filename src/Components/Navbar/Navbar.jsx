27caad6d49e73e02b49e2a7129fe91acabf0279b
claudiahW <murungiclaud@gmail.com>
2024-07-08 17:40:05 +0300

    add navigation menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Navbar.css";
-import logo from '../../assets/bird_2.jpg';
+import logo from "../../assets/bird_2.jpg";
 
 const Navbar = () => {
   return (
-  <div className="navbar">
-<img src={logo} alt="" />
-    
-  </div>)
+    <div className="navbar">
+      <img src={logo} alt="" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Porfolio</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
