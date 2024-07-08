1feb225606b66d8f1bfa74b131a05439b67a0a23
claudiahW <murungiclaud@gmail.com>
2024-07-08 20:36:26 +0300

    start styling

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from './assets/claudiah1.jpeg'
+import React from "react";
+import "./Hero.css";
+import profile_img from "./assets/claudiah1.jpeg";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <img src={profile_img} alt="" />
-    </div>
-  )
-}
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>I'm Claudiah Wanjiru, frontend developer based in Kenya. </h1>
+      <p>
+        I am a frontend developer from Nairobi, Kenya with 4 years experience in
+        frontend technologies
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me!</div>
+        <div className="hero-resume">My Resume</div>
 
-export default Hero
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
