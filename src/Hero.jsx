51f9056b2fbe7393a4c24f446f78da656c2a2706
claudiahW <murungiclaud@gmail.com>
2024-07-08 20:24:50 +0300

    add profile image

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from './assets/claudiah1.jpeg'
 
 const Hero = () => {
   return (
-    <div className='hero'></div>
+    <div className='hero'>
+        <img src={profile_img} alt="" />
+    </div>
   )
 }
 
