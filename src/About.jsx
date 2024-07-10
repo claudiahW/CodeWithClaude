470e4453940e427dd8acc296aba5ab1f3b8d61f3
claudiahW <murungiclaud@gmail.com>
2024-07-10 11:04:31 +0300

    complete styling about section

---
+++ src/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./About.css";
+import theme_pattern from "./assets/cloud.svg";
+import profile_img from "./assets/claudiah8.jpeg";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am an experienced frontend developer with a passion for web
+              development.{" "}
+            </p>
+            <p>
+              I have a strong background in web development, having worked as
+              both a front-end and back-end developer for several years. My
+              skills include HTML, CSS, JavaScript, ReactJS, NodeJS, PHP, and
+              MySQL. I am also familiar with popular frameworks such as
+              Bootstrap and Laravel.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+
+            <div className="about-skill">
+              <p>React JS</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>PHP</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>4+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>40+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
 import Hero from "./Hero.jsx";
+import About from "./About.jsx";
 
 const App = () => {
   return (
-    <>
+    <div>
       <Navbar />
       <Hero />
-    </>
+      <About />
+    </div>
   );
 };
 
