2f30d64cd45408a1f1f76606d3db42b4d81a423b
claudiahW <murungiclaud@gmail.com>
2024-07-10 14:55:14 +0300

    start styling services component

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Services.css'
 import theme_pattern from './assets/claudiah2.jpeg'
+import Services_Data from './assets/services_data.js'
+import arrow_icon from './assets/arrow.svg'
 
 const Services = () => {
   return (
@@ -9,7 +11,19 @@ const Services = () => {
     <h1>My Services</h1> 
     <img src={theme_pattern} alt="" />
 </div>
-
+<div className="services-container">
+   {Services_Data.map((service,index)=>{
+return <div key={index} className="services-format">
+<h3>{service.s_no}</h3>
+<h2>{service.s_name}</h2>
+<p>{service.s_desc}</p>
+<div className='services-readmore'>
+  <p>Read More</p>
+  <img src={arrow_icon} alt="" />
+</div>
+</div>
+   })}
+</div>
     </div>
   )
 }
---
+++ src/assets/services_data.js
@@ -0,0 +1,25 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Web Design",
+        s_desc:"Web design refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development."
+    },
+    {
+        s_no:"02",
+        s_name:"Graphics Design",
+        s_desc:"Web design refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development."
+    },
+    {
+        s_no:"03",
+        s_name:"Social Media",
+        s_desc:"Web design refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development."
+    },
+    {
+        s_no:"04",
+        s_name:"App Design",
+        s_desc:"Web design refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development."
+    }
+
+];
+
+export default Services_Data 
