e7ebfbb675b27db58705eb4a3582be4216b0d503
claudiahW <murungiclaud@gmail.com>
2024-07-17 20:01:05 +0300

    finish styling about me section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -12,14 +12,10 @@ const Hero = () => {
       </h1>
       <p>
         I am a frontend developer from Nairobi, Kenya with 4 years experience in
-        frontend technologies
+        frontend technologies.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">
-          <AnchorLink className="anchor-link" offset={50} href="#contact">
-            Connect With Me
-          </AnchorLink>
-        </div>
+        <div className="hero-connect">Connect With Me</div>
         <div className="hero-resume">My Resume</div>
       </div>
     </div>
