39f5d545fefb678bb8d3f2f6cdc23eb8fc47e2ef
claudiahW <murungiclaud@gmail.com>
2024-07-28 18:40:00 +0300

    mount navbar to app component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,37 +1,13 @@
-import React, { useRef } from "react";
-import "./Navbar.css";
-import logo from "./assets/cloudbackup.svg";
-import underline_icon from "./assets/emailicon.svg";
-import AnchorLink from "react-anchor-link-smooth-scroll";
-import menu_open from "./assets/letter-c.png";
-import menu_close from "./assets/letter-c.png";
+import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <img src={logo} alt="" id="navicon"/>
+    <div className='navbar'>
 
-      <ul className="nav-menu">
-        <li>
-          <p onClick={() => setMenu("home")}>Home</p>
-        </li>
-        <li>
-          <p onClick={() => setMenu("about")}>About Me</p>
-        </li>
-        <li>
-          <p onClick={() => setMenu("services")}>Services</p>
-        </li>
-        <li>
-          <p onClick={() => setMenu("home")}>Porfolio</p>
-        </li>
-        <li>
-          <p onClick={() => setMenu("contact")}>Contact</p>
-        </li>
-      </ul>
-
-      <div className="nav-connect">Connect With Me</div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
