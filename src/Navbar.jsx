7a9b73ed6c31785ae4c4c59eefd75838778c9e11
claudiahW <murungiclaud@gmail.com>
2024-07-31 18:23:12 +0300

    add anchor link class

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "./assets/mylogo1.svg";
 import underline from "./assets/underline.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
@@ -10,23 +11,38 @@ const Navbar = () => {
       <img src={logo} alt="" />
       <ul className="nav-menu">
         <li>
-          <p onClick={()=>setMenu("home")}>Home</p>
+          <AnchorLink className="anchor-link">
+            <p onClick={() => setMenu("home")}>Home</p>
+          </AnchorLink>
+
           {menu === "home" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p onClick={()=>setMenu("about")}>About Me</p>
+          <AnchorLink className="anchor-link">
+            <p onClick={() => setMenu("about")}>About Me</p>
+          </AnchorLink>
+
           {menu === "about" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p  onClick={()=>setMenu("services")}>Services</p>
+          <AnchorLink className="anchor-link">
+            <p onClick={() => setMenu("services")}>Services</p>
+          </AnchorLink>
+
           {menu === "services" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p  onClick={()=>setMenu("work")}>Portfolio</p>
+          <AnchorLink className="anchor-link">
+            <p onClick={() => setMenu("work")}>Portfolio</p>
+          </AnchorLink>
+
           {menu === "work" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p  onClick={()=>setMenu("contact")}>Contact</p>
+          <AnchorLink className="anchor-link">
+            <p onClick={() => setMenu("contact")}>Contact</p>
+          </AnchorLink>
+
           {menu === "contact" ? <img src={underline} /> : <></>}
         </li>
       </ul>
