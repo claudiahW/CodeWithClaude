055f76abd7c4cecb19b4e58a5307c0bbd3dfd7a7
claudiahW <murungiclaud@gmail.com>
2024-07-31 18:56:51 +0300

    change nav link text color to white

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,35 +11,35 @@ const Navbar = () => {
       <img src={logo} alt="" />
       <ul className="nav-menu">
         <li>
-          <AnchorLink className="anchor-link">
+          <AnchorLink className="anchor-link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
           </AnchorLink>
 
           {menu === "home" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50}>
+          <AnchorLink className="anchor-link" offset={50} href="#about">
             <p onClick={() => setMenu("about")}>About Me</p>
           </AnchorLink>
 
           {menu === "about" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50}>
+          <AnchorLink className="anchor-link" offset={50} href="#services">
             <p onClick={() => setMenu("services")}>Services</p>
           </AnchorLink>
 
           {menu === "services" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50}>
+          <AnchorLink className="anchor-link" offset={50} href="#work">
             <p onClick={() => setMenu("work")}>Portfolio</p>
           </AnchorLink>
 
           {menu === "work" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50}>
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
             <p onClick={() => setMenu("contact")}>Contact</p>
           </AnchorLink>
 
