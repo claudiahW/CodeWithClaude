0bbebbdb9b04423f249e6fcded61d6548885898e
claudiahW <murungiclaud@gmail.com>
2024-07-31 20:06:34 +0300

    style menu icon to be responsive to tablet width

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,13 +3,17 @@ import "./Navbar.css";
 import logo from "./assets/mylogo1.svg";
 import underline from "./assets/underline.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "./assets/menu_open.png";
+import menu_close from "./assets/close.png";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
       <img src={logo} alt="" />
+      <img src={menu_open} alt="" className="nav-mob-open" />
       <ul className="nav-menu">
+        <img src={menu_close} alt="" className="nav-mob-close" />
         <li>
           <AnchorLink className="anchor-link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
@@ -46,8 +50,12 @@ const Navbar = () => {
           {menu === "contact" ? <img src={underline} /> : <></>}
         </li>
       </ul>
-      <div className="nav-connect"> <AnchorLink className="anchor-link" offset={50} href="#contact">Connect With Me
-          </AnchorLink></div>
+      <div className="nav-connect">
+        {" "}
+        <AnchorLink className="anchor-link" offset={50} href="#contact">
+          Connect With Me
+        </AnchorLink>
+      </div>
     </div>
   );
 };
