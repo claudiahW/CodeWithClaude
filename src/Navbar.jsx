f8827597c5915ab268a8eb80b0cceb14030f89e1
claudiahW <murungiclaud@gmail.com>
2024-07-31 17:51:34 +0300

    import underline image

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "./assets/mylogo1.svg";
+import underline from "./assets/underline.svg"
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
       <img src={logo} alt="" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li><p>Home</p></li>
+        <li><p>About Me</p></li>
+        <li><p>Services</p></li>
+        <li><p>Portfolio</p></li>
+        <li><p>Contact</p></li>
       </ul>
       <div className="nav-connect">Connect With Me</div>
     </div>
